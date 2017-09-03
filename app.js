67709a32ff990e3a59765e0437284ea403408934
mingsterism <ming.k@hotmail.com>
2017-09-03 08:15:15 +0000

    Fix github login. Get user data

--- app.js
+++ app.js
@@ -8,20 +8,36 @@ const app = express()
 const rString = randomString.generate();
 console.log("Random String -----", rString)
 
+const CLIENT_ID = 123
+const CLIENT_SECRET = 12344
+
 const githubOauthParams = {
-  "client_id": "e03a564f48f8a2ed28b4", 
-  "redirect_uri": "https://coders-map-visualisation.herokuapp.com/callback",
+  "client_id": CLIENT_ID, 
+  //"redirect_uri": "https://coders-map-visualisation.herokuapp.com/callback",
+  "redirect_uri": "http://54.255.249.246:5000/callback", 
   "state": rString,
 }
 
 const githubPostParams = (codeString) => {
    const postObj = {
-      "client_id": "e03a564f48f8a2ed28b4", 
-      "client_secret": "130c08d6c7fa6bea7119ba607f96ba7baad7e1c8",
+      "client_id": CLIENT_ID, 
+      "client_secret": CLIENT_SECRET
       "code": codeString, 
   }
    return postObj;
 }
+
+const requestOptions = (OAUTH_TOKEN) => {
+   const payload = {
+      url: "https://api.github.com/user", 
+      headers: {
+      "Authorization": "token " + OAUTH_TOKEN,
+      'User-Agent': 'request', 
+      }
+   }
+    console.log("PAYLOAD --------", payload)
+  return payload
+  }
 
 app.get('/', (req, res) => {
   console.log("Initializing Request ...", Object.keys(req));
@@ -33,44 +49,34 @@ app.get('/', (req, res) => {
 
 const middle1 = (req, res, next) => {
   console.log("===================== middle1")
-  console.log('res.locals: ', res.locals)
-  console.log('req.locals:', req.locals)
   next() 
 }
 
 const middle2 = (req, res, next) => {
   console.log("--------------------- middle2")
-  console.log('res.locals: ', res.locals)
-  console.log('req.locals:', req.locals)
   next()
 }
 
 app.get('/callback', middle1, middle2, (req, res, next) => {
-  console.log("##############################################")
-  console.log(req.body)
-  console.log(Object.keys(req))
   request.post({ url:"https://github.com/login/oauth/access_token", 
    form: githubPostParams(req.query.code)}, 
    (err, httpResponse, body) => {
-      const stringSplit = body.split("=")
-      console.log(stringSplit)
-      console.log(httpResponse)
-      res.locals.accesscode = body
+    console.log('----------------------')
+     TOKEN = httpResponse.body.split("=")[1]
+      TOKEN_FINAL = TOKEN.split("&")[0]
+    console.log("============================")
+      request(requestOptions(TOKEN_FINAL), (err, resp, body) => {
+        console.log("Body: ------ ", body)
+      })
    }
   )
-
-  const data = request.get({url: "https://api.github.com/user?"+req.query.body}, 
-    (err, httpResponse, body) => {
-      return body
-    })
-  console.log('@@@@@@@@@@@@@@@', data)
-  res.send(data)
+  res.send("IT is the end")
 });
 
 app.get('/redirected', (req, res) => {
   console.log("Calling redirected", Object.keys(req))
 })
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Listening on port", process.env.PORT)
+app.listen(5000, () => {
+  console.log("Listening on port", 5000)
 })
