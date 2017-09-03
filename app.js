67ba5ee24bc21aedc5d693105065951143adf17e
mingsterism <ming.k@hotmail.com>
2017-09-03 07:12:57 +0000

    Fix bugs

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const middle2 = (req, res, next) => {
 app.get('/callback', middle1, middle2, (req, res, next) => {
   console.log("##############################################")
   console.log(req.body)
-  console.log(Object.keys(req)
+  console.log(Object.keys(req))
   request.post({ url:"https://github.com/login/oauth/access_token", 
    form: githubPostParams(req.query.code)}, 
    (err, httpResponse, body) => {
